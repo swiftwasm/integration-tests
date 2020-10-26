af0c80d4f304e2e678f0fc5992cb34116434c6bb
Yuta Saito <kateinoigakukun@gmail.com>
2020-10-26 22:01:53 +0900

    Link CoreFoundation explicitly

--- TestCases/swiftpm-build/test.sh
+++ TestCases/swiftpm-build/test.sh
@@ -5,6 +5,7 @@ source_path="$(cd "$(dirname $0)" && pwd)"
 work_dir="$(mktemp -d)"
 (cd "$work_dir" && \
   $TOOLCHAIN/usr/bin/swift package init --name Example && \
+  echo "import CoreFoundation" >> Tests/LinuxMain.swift && \
   $TOOLCHAIN/usr/bin/swift build --triple wasm32-unknown-wasi -Xswiftc -static-stdlib && \
   $TOOLCHAIN/usr/bin/swift package clean && \
   $TOOLCHAIN/usr/bin/swift build --triple wasm32-unknown-wasi -Xswiftc -static-stdlib --build-tests
