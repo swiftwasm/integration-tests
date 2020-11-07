094cf07e279795671d3096fcf1e319d68b7e3976
Yuta Saito <kateinoigakukun@gmail.com>
2020-11-07 18:22:39 +0900

    Test SwiftPM with system-module imports

--- TestCases/swiftpm-build/test.sh
+++ TestCases/swiftpm-build/test.sh
@@ -5,8 +5,10 @@ source_path="$(cd "$(dirname $0)" && pwd)"
 work_dir="$(mktemp -d)"
 (cd "$work_dir" && \
   $TOOLCHAIN/usr/bin/swift package init --name Example && \
-  echo "import CoreFoundation" >> Tests/LinuxMain.swift && \
+  cp "$source_path/../import-system-modules/main.swift" Sources/Example/Imports.swift && \
   $TOOLCHAIN/usr/bin/swift build --triple wasm32-unknown-wasi -Xswiftc -static-stdlib && \
   $TOOLCHAIN/usr/bin/swift package clean && \
-  $TOOLCHAIN/usr/bin/swift build --triple wasm32-unknown-wasi -Xswiftc -static-stdlib --build-tests
+  $TOOLCHAIN/usr/bin/swift build --triple wasm32-unknown-wasi -Xswiftc -static-stdlib --build-tests && \
+  $TOOLCHAIN/usr/bin/swift package clean && \
+  $TOOLCHAIN/usr/bin/swift build --triple wasm32-unknown-wasi --build-tests
 )
