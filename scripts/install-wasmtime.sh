b807b4d45f9e61dca89895e9ea6c21fccc915357
Yuta Saito <kateinoigakukun@gmail.com>
2022-03-10 08:52:30 +0900

    Fix wasmtime installer

--- scripts/install-wasmtime.sh
+++ scripts/install-wasmtime.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+set -ue
+
 BIN_PATH="$1"
 export WASMTIME_HOME=$(mktemp -d)
-curl https://wasmtime.dev/install.sh --fail --location | bash
+curl https://wasmtime.dev/install.sh --fail --location | bash -s -- --version v0.35.1
 mkdir -p "$BIN_PATH"
 mv "$WASMTIME_HOME/bin/"* "$BIN_PATH/"
