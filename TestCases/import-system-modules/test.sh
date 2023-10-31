46f03d4bee22e1263361113de857ef3af96a4617
Yuta Saito <kateinoigakukun@gmail.com>
2023-10-31 13:40:42 +0000

    Require explicit -sdk and remove wasmer test
    wasmer does not support socket wasi API

--- TestCases/import-system-modules/test.sh
+++ TestCases/import-system-modules/test.sh
@@ -6,9 +6,8 @@ work_dir="$(mktemp -d)"
 # FIXME: The current SDK layout is wrong, so -resource-dir is necessary here. Swift SDK should be overlayed on wasi-sysroot.
 (cd "$work_dir" && \
   $TOOLCHAIN/usr/bin/swiftc -resource-dir $TOOLCHAIN/usr/lib/swift_static -target wasm32-wasi $source_path/main.swift -sdk $TOOLCHAIN/usr/share/wasi-sysroot -static-stdlib -o main.wasm && \
-  $TOOLCHAIN/usr/bin/swiftc -resource-dir $TOOLCHAIN/usr/lib/swift_static -target wasm32-wasi $source_path/main.swift -static-stdlib -o main.wasm && \
-  $TOOLCHAIN/usr/bin/swiftc -resource-dir $TOOLCHAIN/usr/lib/swift_static -target wasm32-wasi $source_path/main.swift -o main.wasm && \
-  wasmer run main.wasm && \
+  # $TOOLCHAIN/usr/bin/swiftc -resource-dir $TOOLCHAIN/usr/lib/swift_static -target wasm32-wasi $source_path/main.swift -static-stdlib -o main.wasm && \
+  # $TOOLCHAIN/usr/bin/swiftc -resource-dir $TOOLCHAIN/usr/lib/swift_static -target wasm32-wasi $source_path/main.swift -o main.wasm && \
   # FIXME: enable wasmtime test after it supports Apple Silicon
   if [ $(uname -m) != "arm64" ]; then wasmtime run main.wasm; fi
 )
--- scripts/install-wasmer.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-BIN_PATH="$1"
-export WASMER_DIR=$(mktemp -d)
-curl https://get.wasmer.io --fail --location | sh
-mkdir -p "$BIN_PATH"
-mv "$WASMER_DIR/bin/"* "$BIN_PATH/"
